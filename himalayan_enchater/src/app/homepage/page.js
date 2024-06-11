84f75ef8889d75a4f06d8bd12f74380fb3a32dfc
shahil somare magar <shahilsomare72@gmail.com>
2024-06-11 11:25:38 +0545

    added custom colors

--- himalayan_enchater/src/app/homepage/page.js
+++ himalayan_enchater/src/app/homepage/page.js
@@ -4,5 +4,4 @@ return (
         Hello from homepage
     </div>
 )
-
 } 
--- himalayan_enchater/tailwind.config.js
+++ himalayan_enchater/tailwind.config.js
@@ -1,3 +1,5 @@
+const { Kaushan_Script } = require("next/font/google");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,10 +9,26 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        kaushanScript: ["Kaushan Script, cursive"],
+      },
+
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        customOrange: {
+          DEFAULT: "#F7941D",
+        },
+        customGray: {
+          DEFAULT: "#23231A",
+        },
+        customPale: {
+          DEFAULT: "#FDEAD4",
+          dark: "#F9DCBB"
+        }
       },
     },
   },
